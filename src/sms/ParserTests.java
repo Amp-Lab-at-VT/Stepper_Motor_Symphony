25bc41a682df63f2c36b8ed1ec29181f694e71af
Dylan Green <dgpokemaster08@gmail.com>
2023-09-19 21:38:59 -0400

    Added percussion parsing tests for Parser class

--- src/sms/ParserTests.java
+++ src/sms/ParserTests.java
@@ -6,8 +6,10 @@ import javax.sound.midi.InvalidMidiDataException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static sms.Percussion.Type;
 
 public class ParserTests {
 
@@ -196,5 +198,96 @@ public class ParserTests {
         assertEquals(150, notes.get(7).startTime());
         assertEquals(48, notes.get(7).duration());
     }
-    
+
+    @Test
+    public void testSimplePercussion() throws InvalidMidiDataException, IOException {
+        Parser p = new Parser();
+        var percussion = p.parseMidi(new File("testfiles/simplePercussion.mid")).second();
+        assertEquals(4, percussion.size());
+
+        assertEquals(0, percussion.get(0).startTime());
+        assertEquals(Type.ACOUSTIC_BASS_DRUM, percussion.get(0).type());
+        assertEquals(50, percussion.get(1).startTime());
+        assertEquals(Type.ACOUSTIC_BASS_DRUM, percussion.get(1).type());
+        assertEquals(100, percussion.get(2).startTime());
+        assertEquals(Type.ACOUSTIC_BASS_DRUM, percussion.get(2).type());
+        assertEquals(150, percussion.get(3).startTime());
+        assertEquals(Type.ACOUSTIC_BASS_DRUM, percussion.get(3).type());
+    }
+
+    @Test
+    public void testPercussion_3parts() throws InvalidMidiDataException, IOException {
+        Parser p = new Parser();
+        var percussion = p.parseMidi(new File("testfiles/percussion_3parts.mid")).second();
+        assertEquals(10, percussion.size());
+
+        var bass = percussion.stream()
+                .filter(command -> command.type() == Type.ACOUSTIC_BASS_DRUM)
+                .toList();
+        var snare = percussion.stream()
+                .filter(command -> command.type() == Type.ACOUSTIC_SNARE)
+                .toList();
+        var hihat = percussion.stream()
+                .filter(command -> command.type() == Type.CLOSED_HI_HAT)
+                .toList();
+
+        assertEquals(4, bass.size());
+        assertEquals(0, bass.get(0).startTime());
+        assertEquals(50, bass.get(1).startTime());
+        assertEquals(100, bass.get(2).startTime());
+        assertEquals(150, bass.get(3).startTime());
+
+        assertEquals(2, snare.size());
+        assertEquals(50, snare.get(0).startTime());
+        assertEquals(150, snare.get(1).startTime());
+
+        assertEquals(4, hihat.size());
+        assertEquals(25, hihat.get(0).startTime());
+        assertEquals(75, hihat.get(1).startTime());
+        assertEquals(125, hihat.get(2).startTime());
+        assertEquals(175, hihat.get(3).startTime());
+    }
+
+    @Test
+    public void testSimpleCombinedNotesAndPercussion() throws InvalidMidiDataException, IOException {
+        Parser p = new Parser();
+        var parsed = p.parseMidi(new File("testfiles/percussionAndNotes.mid"));
+        var notes = parsed.first();
+        var percussion = parsed.second();
+        assertEquals(8, notes.size());
+        assertEquals(6, percussion.size());
+
+        assertEquals(0, notes.get(0).startTime());
+        assertEquals(25, notes.get(1).startTime());
+        assertEquals(50, notes.get(2).startTime());
+        assertEquals(75, notes.get(3).startTime());
+        assertEquals(100, notes.get(4).startTime());
+        assertEquals(125, notes.get(5).startTime());
+        assertEquals(150, notes.get(6).startTime());
+        assertEquals(175, notes.get(7).startTime());
+
+        for (Note n : notes) {
+            assertEquals(23, n.duration());
+        }
+
+        var bass = percussion.stream()
+                .filter(command -> command.type() == Type.ACOUSTIC_BASS_DRUM)
+                .toList();
+        var snare = percussion.stream()
+                .filter(command -> command.type() == Type.ELECTRIC_SNARE)
+                .toList();
+
+        assertEquals(4, bass.size());
+        assertEquals(2, snare.size());
+
+        assertEquals(4, bass.size());
+        assertEquals(0, bass.get(0).startTime());
+        assertEquals(50, bass.get(1).startTime());
+        assertEquals(100, bass.get(2).startTime());
+        assertEquals(150, bass.get(3).startTime());
+
+        assertEquals(2, snare.size());
+        assertEquals(50, snare.get(0).startTime());
+        assertEquals(150, snare.get(1).startTime());
+    }
 }
